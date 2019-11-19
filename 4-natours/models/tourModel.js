7d79cf307de19cf9a7a24a0f959577dbd72bd0aa
Eero Suvanto <ejsuva@utu.fi>
2019-11-20 00:18:21 +0200

    Started auth and added token

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -1,40 +1,40 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const validator = require('validator');
+// const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: [ true, 'A tour must have a name' ],
+      required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
-      maxlength: [ 40, 'A tour must have less or equal than 40 characters' ],
-      minlength: [ 5, 'A tour must have more or equal than 5 characters' ]
+      maxlength: [40, 'A tour must have less or equal than 40 characters'],
+      minlength: [5, 'A tour must have more or equal than 5 characters']
       // validate: [ validator.isAlpha, 'Tour name must only contain characters' ]
     },
     slug: String,
     duration: {
       type: Number,
-      required: [ true, 'A tour must have a duration' ]
+      required: [true, 'A tour must have a duration']
     },
     maxGroupSize: {
       type: Number,
-      required: [ true, 'A tour must have a group size' ]
+      required: [true, 'A tour must have a group size']
     },
     difficulty: {
       type: String,
-      required: [ true, 'A tour must have a difficulty' ],
+      required: [true, 'A tour must have a difficulty'],
       enum: {
-        values: [ 'easy', 'medium', 'difficult' ],
+        values: ['easy', 'medium', 'difficult'],
         message: 'Difficulty is either: easy, medium, difficult'
       }
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
-      min: [ 1, 'Rating must be above 1.0' ],
-      max: [ 5, 'Rating must be below 5.0' ]
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0']
     },
     ratingsQuantity: {
       type: Number,
@@ -43,7 +43,7 @@ const tourSchema = new mongoose.Schema(
     price: {
       type: Number,
       // eslint-disable-next-line prettier/prettier
-      required: [ true, 'A tour must have a price' ]
+      required: [true, 'A tour must have a price']
     },
     priceDiscount: {
       type: Number,
@@ -58,7 +58,7 @@ const tourSchema = new mongoose.Schema(
     summary: {
       type: String,
       trim: true,
-      required: [ true, 'A tour must have a description' ]
+      required: [true, 'A tour must have a description']
     },
     description: {
       type: String,
@@ -66,15 +66,15 @@ const tourSchema = new mongoose.Schema(
     },
     imageCover: {
       type: String,
-      required: [ true, 'A tour must have a cover image' ]
+      required: [true, 'A tour must have a cover image']
     },
-    images: [ String ],
+    images: [String],
     createdAt: {
       type: Date,
       default: Date.now(),
       select: false
     },
-    startDates: [ Date ],
+    startDates: [Date],
     secretTour: {
       type: Boolean,
       default: false
--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
@@ -20,15 +26,17 @@ mongoose
     useFindAndModify: false,
     useUnifiedTopology: true
   })
-  // eslint-disable-next-line prettier/prettier
-  .then((con) => {
-    // eslint-disable-next-line no-console
-    console.log('DB connection successful');
-  });
-
+  .then(() => console.log('DB connection successful'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  // eslint-disable-next-line no-console
+const server = app.listen(port, () => {
   console.log(`App is running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
