dbdbc749ac8c6cf92a31f7de973eb2d48b368fad
Eero Suvanto <ejsuva@utu.fi>
2019-11-26 23:37:14 +0200

    Heroku deploy

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -57,7 +57,7 @@ exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
   req.query.sort = '-ratingsAverage,price';
   req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
-  console.log(req.query);
+  // console.log(req.query);
   next();
 };
 
